ddfd8f1f2d6db779766c0c7a46fae71012648fbf
Thomas Gagne <thomasgagne100@gmail.com>
2015-10-18 13:27:48 -0700

    Changed showPassengers.php to have the same CSS styling as the other pages.

--- showPassengers.php
+++ showPassengers.php
@@ -1,27 +1,41 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>List Passengers</title>
+    <link rel="stylesheet" type="text/css" href="css/basic.css">
+  </head>
 
-     try
-     {
-         //open the sqlite database file
-         $db = new PDO('sqlite:database/airport.db');
- 
-         // Set errormode to exceptions
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-         //select all passengers
-         $result = $db->query('SELECT * FROM passengers');
- 
-         //loop through each tuple in result set
-         foreach($result as $tuple)
+  <body>
+
+    <div class="content">
+      <?php
+         try
          {
-              echo "$tuple[ssn] $tuple[f_name] $tuple[l_name]<br/>";
-         }
+            //open the sqlite database file
+            $db = new PDO('sqlite:database/airport.db');
  
-         //disconnect from database
-         $db = null;
-     }
-     catch(PDOException $e)
-     {
-         echo 'Exception : '.$e->getMessage();
-     }
-?>
+            // Set errormode to exceptions
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+            //select all passengers
+            $result = $db->query('SELECT * FROM passengers');
+ 
+            //loop through each tuple in result set
+            foreach($result as $tuple)
+            {
+              echo "$tuple[ssn] $tuple[f_name] $tuple[l_name]<br/>";
+            }
+ 
+            //disconnect from database
+            $db = null;
+         }
+         catch(PDOException $e)
+         {
+           echo 'Exception : '.$e->getMessage();
+         }
+      ?>
+    </div>
+
+  </body>
+
+</html>
