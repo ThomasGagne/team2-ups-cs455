bf9d2f9f2bb943f48178f90cbdae64460fab141e
EC2 Default User <ec2-user@ip-172-31-56-139.ec2.internal>
2015-12-06 23:52:42 +0000

    Some changes to uploadTestHandler, I dunno I didn't make them

--- uploadTestHandler.php
+++ uploadTestHandler.php
@@ -16,24 +16,42 @@ echo $albumName . " " . $artist;*/
 
 //add files to the /songs/ directory
 
-//Loop through each file
-for($i=0; $i<count($_FILES['upload']['name']); $i++) {
-  //Get the temp file path
-  $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+$valid_formats = array("mp3", "ogg", "wav");
+$max_file_size = 1024*100000; //100 kb
+$path = "songs/"; // Upload directory
+$count = 0;
 
-  //Make sure we have a filepath
-  if ($tmpFilePath != ""){
-    //Setup our new file path
-    $newFilePath = "./songs/" . $_FILES['upload']['name'][$i];
-
-    //Upload the file into the temp dir
-    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-
-      //Handle other code here
-
-    }
-  }
-}
+if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
+	echo("Success 1!");
+	// Loop $_FILES to exeicute all files
+	foreach ($_FILES['upload']['name'] as $f => $name) {     
+	    echo("Success: " . $name);
+	    if ($_FILES['upload']['error'][$f] == 4) {
+	        echo("Fail 1!");
+	        continue; // Skip file if any error found
+	    };
+	    echo("Made it here");
+	    echo($_FILES['upload']['error'][$f]);
+	    if ($_FILES['upload']['error'][$f] == 0) {
+	    		echo("Success 2!");        
+	        if ($_FILES['upload']['size'][$f] > $max_file_size) {
+	            $message[] = "$name is too large!.";
+	            continue; // Skip large files
+	        	echo("error: " . $message);
+	        }
+			elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $valid_formats) ){
+				$message[] = "$name is not a valid format";
+				continue; // Skip invalid file formats
+				echo("error: " . $message);
+			}
+	        else{ // No error found! Move uploaded files 
+	            if(move_uploaded_file($_FILES["upload"]["tmp_name"][$f], $path.$name))
+	            $count++; // Number of successfully uploaded file
+	        	echo("Success!");
+	        };
+	    };
+	};
+};
 
 
 
