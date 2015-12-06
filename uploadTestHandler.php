680813b8e79ad799d9888fb93643568ab34615d4
EC2 Default User <ec2-user@ip-172-31-56-139.ec2.internal>
2015-12-06 23:03:55 +0000

    Trying another commit, I'm not sure the last one worked properly

--- uploadTestHandler.php
+++ uploadTestHandler.php
@@ -6,20 +6,74 @@ $tagArray = $_POST['tags'];
 $titleArray = $_POST['titles'];
 $fileArray = $_FILES['upload']['name'];
 
-//Loop through each file
-/*if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
-  for($i=0; $i<count($_FILES['upload']['name']); $i++) {
-    $fileName = $_FILES['upload']['name'][$i];
-    echo($fileName);
-  };
-};*/
-
-
 //var_dump($fileArray);
-echo(" ");
+/*echo(" ");
 var_dump($tagArray);
 echo(" ");
 //echo($tagArray);
-echo $albumName . " " . $artist;
+echo $albumName . " " . $artist;*/
+
+
+//add files to the /songs/ directory
+
+//Loop through each file
+for($i=0; $i<count($_FILES['upload']['name']); $i++) {
+  //Get the temp file path
+  $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+  //Make sure we have a filepath
+  if ($tmpFilePath != ""){
+    //Setup our new file path
+    $newFilePath = "./songs/" . $_FILES['upload']['name'][$i];
+
+    //Upload the file into the temp dir
+    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+
+      //Handle other code here
+
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+try {
+  //connect to the database
+  $db = new PDO('sqlite:database/airport.db');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} catch(PDOException $e) {
+    echo 'Exception : '.$e->getMessage();
+}
+
 
 ?>
