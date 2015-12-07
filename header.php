ced5c29d0ae1256add84164751e64b2d644dbfc3
nmurray <n.murray53@yahoo.com>
2015-12-06 18:00:39 -0800

    re-added home button to header

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <header>
     <div class="header">
+        <!-- Display Username or login -->
         <span class="headerUsername">
             <?php
             if (isset($_SESSION["username"])) {
@@ -18,11 +19,13 @@
             |
         </span>
         <span>
+            <!-- Search Songs -->
             <form class="headerSearchContainer" action="/searchSongs.php">
                 <input type="text" name="searchSongs" class="headerSearch" placeholder="Search Songs" size="30"/>
                 <input type="submit" style="font-size: 12px;" value="Search Songs">
             </form>
         </span>
+        <!-- Search Playlists -->
         <span>
             <form class="headerSearchContainer" action="/searchPlaylists.php">
                 <input type="text" name="searchPlaylists" class="headerSearch" placeholder="Search Playlists" size="30"/>
@@ -32,6 +35,14 @@
         <span class="headerUsername">
             <a href="/searchHelp.php" style="font-size: 14px; margin-left: -10px;">Search Help</a>
         </span>
+        <!-- Home button -->
+        <span class="indexHome">
+            <?php
+            echo '<a href="/index.php">Home</a>';
+            echo "  |   ";
+            ?>
+        </span>
+        <!-- Logout button -->
         <span class="headerLogout">
             <?php
             if (isset($_SESSION["username"])) {
